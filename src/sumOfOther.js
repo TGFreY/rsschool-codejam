c2d8f5453d146f37ffc98078fc13de7640942c0e
TGFreY <t.g.frey19@gmail.com>
2018-10-28 19:10:14 +0300

    refactor: a code of "sumOfOthers" change for tests

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,7 +1,4 @@
-const sumOfOther = (arr) => {
+module.exports = function sumOfOther(arr) {
   const sum = arr.reduce((acc, cur) => acc + cur);
   return arr.map(elem => sum - elem);
 };
-
-const arr = [2, 3, 4, 1];
-console.log(sumOfOther(arr));
